ac7c5f430e2734d1002ee85f97bbb0439023a4a0
gabrielacorbalan <gabrielacorbalan@localhost>
2021-03-07 16:31:49 +0100

    inicio

---
+++ src/trobify/InicioController.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trobify;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author gabrielacorbalan
+ */
+public class InicioController implements Initializable {
+
+    @FXML
+    private Button comprarBoton;
+    @FXML
+    private Button alquilarBoton;
+    @FXML
+    private Button compartirBoton;
+    @FXML
+    private ComboBox<?> tipo;
+    @FXML
+    private TextField ciudadText;
+    @FXML
+    private Button iniciaBoton;
+    @FXML
+    private Hyperlink registrarse;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void comprar(ActionEvent event) {
+    }
+
+    @FXML
+    private void alquilar(ActionEvent event) {
+    }
+
+    @FXML
+    private void compartir(ActionEvent event) {
+    }
+
+    @FXML
+    private void inicia(ActionEvent event) {
+    }
+    
+}
