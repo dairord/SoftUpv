32b2106966cff0207868b8f69124548a1fb58aaa
Jagondia96 <jagondia96@gmail.com>
2021-03-07 16:50:06 +0100

    Carpetas nuevas

--- src/trobify/controlador/InicioController.java
+++ src/trobify/controlador/InicioController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package trobify;
+package trobify.controlador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
