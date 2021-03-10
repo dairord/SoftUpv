e25f1b078d7c9b3af06c4faa4893bb2521328c21
gabrielacorbalan <gabrielacorbalan@localhost>
2021-03-10 11:59:27 +0100

    fkoap

--- src/trobify/controlador/BuscadorController.java
+++ src/trobify/controlador/BuscadorController.java
@@ -5,6 +5,7 @@
  */
 package trobify.controlador;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -12,11 +13,15 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -49,7 +54,7 @@ public class BuscadorController implements Initializable {
     private ComboBox<String> variacionFiltros;
     @FXML
     private TextField numHabitaciones;
-
+    
     /**
      * Initializes the controller class.
      */
@@ -61,8 +66,18 @@ public class BuscadorController implements Initializable {
      tiposViviendas.add("Casa");
     ObservableList<String> viviendas = FXCollections.observableList(tiposViviendas);
     tipoVivienda.setItems(viviendas);
+    
+    //ordenar por
+      ArrayList <String> orden = new ArrayList <String> ();
+     orden.add("Relevancia");
+     orden.add("Precio más bajo");
+     orden.add("Precio más alto");
+    ObservableList<String> ordenar = FXCollections.observableList(orden);
+    ordenarPor.setItems(ordenar);
+    ordenarPor.getSelectionModel().selectFirst();
     }    
-    //
+    
+    
     @FXML
     private void guardarFiltros(ActionEvent event) {
     }
@@ -73,6 +88,22 @@ public class BuscadorController implements Initializable {
 
     @FXML
     private void buscar(ActionEvent event) {
+    }
+
+    @FXML
+    private void Inicio(ActionEvent event) throws IOException {
+         FXMLLoader loader = new FXMLLoader(getClass().getResource("/trobify/views/Inicio.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+      //  scene.getStylesheets().add(this.getClass().getResource("/resources/anadir.css").toExternalForm());
+        Stage stage = new Stage();
+       
+
+        stage.setScene(scene);
+        stage.setTitle("Trobify");
+
+        stage.showAndWait();
     }
     
 }
