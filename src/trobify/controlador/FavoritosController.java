2f4581dc8a4bd0e1b7322a8037350edc9bc23231
Jagondia96 <jagondia96@gmail.com>
2021-03-21 16:46:03 +0100

    Cosas varias

--- src/trobify/controlador/FavoritosController.java
+++ src/trobify/controlador/FavoritosController.java
@@ -5,15 +5,27 @@
  */
 package trobify.controlador;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
@@ -22,6 +34,7 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import trobify.Conectar;
 
 /**
  * FXML Controller class
@@ -33,62 +46,262 @@ public class FavoritosController implements Initializable {
     @FXML
     private Label nombreUsuario;
     @FXML
-    private ChoiceBox<String> elegirOrden;
-    @FXML
     private VBox listaViviendas;
-
+    @FXML
+    private ComboBox<String> elegirOrdenPor;
     private static Stage s;
     /**
      * Initializes the controller class.
      */
+    
+    Conectar con;
+    ArrayList<String> favList;
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
-        //Mostrar nombre de usuario
-        nombreUsuario.setText("Patricio");
+        //Crear una conexion
+        con = new Conectar();        
         
         // Seleccion del orden de busqueda
-        ArrayList orden = new ArrayList();
+        ArrayList<String> orden = new ArrayList();
         orden.add("Relevancia");
         orden.add("Precio más bajo");
         orden.add("Precio más alto");
+        orden.add("Valoracion");
         
         ObservableList<String> ordenar = FXCollections.observableList(orden);
-        elegirOrden.setItems(ordenar);
+        elegirOrdenPor.setItems(ordenar);
         
-        elegirOrden.getSelectionModel().selectFirst();
+        elegirOrdenPor.getSelectionModel().selectFirst();
         
-        //Lista de viviendas        
-        for (int i = 0; i < 5; ++i) {
-        this.listaViviendas.getChildren().add(crearMiniatura());
+        //Crear Array con la lista de favoritos, por defecto seleccionara LA PRIMERA opcion
+        favList = new ArrayList();       
+        switch (elegirOrdenPor.getSelectionModel().selectedItemProperty().getValue()) {
+            case "Relevancia":
+                ordenarPrecio();
+                break;
+            case "Precio más bajo":
+                ordenarPrecioBajo();
+                break;
+            case "Precio más alto":
+                ordenarPrecioAlto();
+                break;
+            case "Valoracion":
+                ordenarValoracion();
+                break;
+            default:
+                break;
         }
+                               
+        //Mostrar nombre de usuario
+        nombreUsuario.setText("Patricio");
         
+        ordenarLista();   
     }    
     
+    
     //Generador de miniauras
-    private javafx.scene.layout.HBox crearMiniatura(){
+    private javafx.scene.layout.HBox crearMiniatura(String id, String rutaFoto, String nombreCalle, int precioVivienda, int valoracionVivienda) throws FileNotFoundException{
         
         javafx.scene.layout.HBox miniatura = new javafx.scene.layout.HBox();
         
         miniatura.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, CornerRadii.EMPTY, Insets.EMPTY)));
         
-        Image image1 = new Image("/trobify/views/_FI.jpg");
+        Button botonRedireccion = new Button();
+        botonRedireccion.setPadding(new Insets(0,0,0,0));
+        botonRedireccion.setId(id);
+        
+        Image image1 = new Image(new FileInputStream(rutaFoto));
         javafx.scene.image.ImageView foto = new javafx.scene.image.ImageView(image1);
         foto.setFitWidth(200);
         foto.setFitHeight(150);
+        
+        botonRedireccion.setGraphic(foto);
                 
         javafx.scene.layout.VBox datos = new javafx.scene.layout.VBox(10);
         datos.setPadding(new Insets(20,30,30,15));
         
-        javafx.scene.control.Label calle = new javafx.scene.control.Label("Calle: " + "Nombre de la calle");
-        javafx.scene.control.Label precio = new javafx.scene.control.Label("Precio: " + "Precui de la vivienda" + "/mes");
-        javafx.scene.control.Label valoracion = new javafx.scene.control.Label("Valoracion: " + "Valoracion de la vivienda");
+        javafx.scene.control.Label calle = new javafx.scene.control.Label("Calle: " + nombreCalle);
+        javafx.scene.control.Label precio = new javafx.scene.control.Label("Precio: " + precioVivienda + "/mes");
+        javafx.scene.control.Label valoracion = new javafx.scene.control.Label("Valoracion: " + valoracionVivienda);
+        if(valoracionVivienda == -1) valoracion.setVisible(false);
+        
         datos.getChildren().addAll(calle,precio,valoracion);
         
-        miniatura.getChildren().addAll(foto, datos);        
+        miniatura.getChildren().addAll(botonRedireccion, datos);        
         return miniatura;
     }
-     public static void pasarStage(Stage m){
-         s = m;
-     }
+    
+    //Consultar la primera foto de la vivienda pasando como atributo un id
+    public String consultarFoto(String id){
+        
+        try {
+            Statement stm = con.getConnection().createStatement();
+            ResultSet rsl = stm.executeQuery("SELECT id FROM fotografia WHERE id_vivienda = '" + id + "'");
+            if (rsl.first()) return rsl.getNString(1);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(InicioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "F:\\PSW\\SoftUpv\\src\\trobify\\images\\foto0.jpg";
+        //"F:\\PSW\\SoftUpv\\src\\trobify\\images\\foto0.jpg"
+    }
+    
+    //Consultar la direccion de la vivienda pasando como atributo un id    
+    public String consultarDireccion(String id){
+        
+        try {
+            Statement stm = con.getConnection().createStatement();
+            ResultSet rsl = stm.executeQuery("SELECT direccion FROM vivienda WHERE id = '" + id + "'");
+            if (rsl.first()) return rsl.getNString(1);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(InicioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "No disponible";
+    }
+    
+    //Consultar el precio de la vivienda pasando como atributo un id    
+    public int consultarPrecio(String id){
+        
+        try {
+            Statement stm = con.getConnection().createStatement();
+            ResultSet rsl = stm.executeQuery("SELECT precio FROM vivienda WHERE id = '" + id + "'");
+            if (rsl.first()) return rsl.getInt(1);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(InicioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 0;
+    }
+    
+    //Consultar la valoracion fijada a la vivienda
+    public int consultarValoracion(String id){
+        
+        int aux = -1;
+        
+        try {
+            Statement stm = con.getConnection().createStatement();
+            ResultSet rsl = stm.executeQuery("SELECT valoracion FROM favoritos WHERE id LIKE '" + id + "'");
+            if (rsl.first()) aux = rsl.getInt(1);
+            if(aux == 0) return -1;
+            else return aux;
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(InicioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return aux;
+    }
+     
+    private void ordenarPrecio(){
+        try {
+            Statement stm = con.getConnection().createStatement();
+            ResultSet rsl = stm.executeQuery ("SELECT id FROM favoritos");
+            if(rsl.first()){
+                rsl.beforeFirst();
+                while (rsl.next()) {
+                    favList.add(rsl.getString("id"));
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(InicioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+     
+    }
+     
+    private void ordenarPrecioBajo(){
+        try {
+            Statement stm = con.getConnection().createStatement();
+            ResultSet rsl = stm.executeQuery ("SELECT f.id FROM favoritos f, vivienda v WHERE f.id = v.id ORDER BY precio ASC");
+            if(rsl.first()){
+                rsl.beforeFirst();
+                while (rsl.next()) {
+                    favList.add(rsl.getString("id"));
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(InicioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+     
+    private void ordenarPrecioAlto(){
+        try {
+            Statement stm = con.getConnection().createStatement();
+            ResultSet rsl = stm.executeQuery ("SELECT f.id FROM favoritos f, vivienda v WHERE f.id = v.id ORDER BY precio DESC");
+            if(rsl.first()){
+                rsl.beforeFirst();
+                while (rsl.next()) {
+                    favList.add(rsl.getString("id"));
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(InicioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private void ordenarValoracion(){
+        try {
+            Statement stm = con.getConnection().createStatement();
+            ResultSet rsl = stm.executeQuery ("SELECT f.id FROM favoritos f, vivienda v WHERE f.id = v.id ORDER BY valoracion DESC");
+            if(rsl.first()){
+                rsl.beforeFirst();
+                while (rsl.next()) {
+                    favList.add(rsl.getString("id"));
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(InicioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+     
+    //Lista de viviendas
+    private void ordenarLista(){
+        for (int i = 0; i < favList.size(); ++i) {
+            
+            String idBoton = "idBoton" +i;
+            
+            try {
+                String foto = consultarFoto(favList.get(i));
+                String calle = consultarDireccion(favList.get(i));
+                int precio = consultarPrecio(favList.get(i));
+                int valoracion = consultarValoracion(favList.get(i));
+                this.listaViviendas.getChildren().add(crearMiniatura(idBoton, foto, calle, precio, valoracion));
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(FavoritosController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    @FXML
+    private void ordenCambiado(ActionEvent event) {
+        
+        favList.clear();
+        
+        switch (elegirOrdenPor.getSelectionModel().selectedItemProperty().getValue()) {
+            case "Relevancia":                
+                listaViviendas.getChildren().clear();
+                ordenarPrecio();
+                ordenarLista();
+                break;
+            case "Precio más bajo":
+                listaViviendas.getChildren().clear();
+                ordenarPrecioBajo();
+                ordenarLista();
+                break;
+            case "Precio más alto":
+                listaViviendas.getChildren().clear();
+                ordenarPrecioAlto();
+                ordenarLista();
+                break;
+            case "Valoracion":
+                listaViviendas.getChildren().clear();
+                ordenarValoracion();
+                ordenarLista();
+                break;
+            default:
+                break;
+        }
+    }
 }
--- src/trobify/controlador/FichaViviendaController.java
+++ src/trobify/controlador/FichaViviendaController.java
@@ -5,22 +5,38 @@
  */
 package trobify.controlador;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import trobify.Conectar;
 
 /**
  * FXML Controller class
@@ -41,26 +57,50 @@ public class FichaViviendaController implements Initializable {
     private VBox recomendados;
 
     private static Stage s;
+    @FXML
+    private Button addFavoritos;
+    @FXML
+    private HBox valoracionGrupo;
+    @FXML
+    private Text textoValoracion;
+    @FXML
+    private Button editarValoracion;
+    @FXML
+    private TextField valorValoracion;
     /**
      * Initializes the controller class.
      */
     
+    Conectar con;
+    String id;
+    Boolean estaEnFav;
+    ArrayList<String> listaFotos;
+    ArrayList<String> listaRecomendados;
+    int valoracion;
+    
     @Override
-    public void initialize(URL url, ResourceBundle rb) {     
+    public void initialize(URL url, ResourceBundle rb) {
         
-        //Boton de volver atras        
-            //volver.setOnAction(e -> scenePropia.setScene(scenaPrevia));
+        //Crear una conexion
+        con = new Conectar();
         
-        //Descripcion de la vivienda
-        descripcion.setText("Redpiso Primado Reig les ofrece esta vivienda en alquiler, "
-                + "DISPONIBLE A PARTIR DE MARZO. La vivienda se encuentra en una primera planta "
-                + "sin ascensor y consta de 3 habitaciones, dos de ellas dobles, salón comedor, cocina "
-                + "independiente con galería, baño completo. El piso cuenta con buena luminosidad y ventilación ya que es completamente exterior.\n" 
-                + "La finca en la que se encuentra la vivienda cuenta con una zona de jardin privado.\n" 
-                + "LOCALIZACIÓN VIVIENDA: A 150 metros de la parada del tranvía, cercana a paradas de autobús, a 200 "
-                + "metros del PARQUE DE VIVEROS y muy próxima al Real Club de Tenis Valencia y La Hípica. Ven a visitar esta vivienda, "
-                + "¡estaremos encantados de conocerte!");
+        //Dando valor a mano de la id de vivienda AQUI SE DEBERA
+        //PASAR EL ID DE LA VIVIENDA DESDE LA VENTANA ANTERIOR
+        this.id = "vivienda2";
         
+        //Mostrar botones de valoraciones de favortitos o no
+        this.estaEnFav = estaEnFavoritos(this.id);
+        estaValorado();
+        mostrarBotones(); 
+        
+        //Crear Array con la lista de fotos de la galeria
+        this.listaFotos = new ArrayList();
+        crearListaFotos(this.id);
+        
+        //Crear Array con la lista de fotos de la galeria
+        this.listaRecomendados = new ArrayList();
+        crearListaRecomendados(this.id);
+                       
         //Servicios cerca de la vivienda        
         ArrayList serviciosCerca = new ArrayList();
         serviciosCerca.add("Supermercado");
@@ -70,23 +110,92 @@ public class FichaViviendaController implements Initializable {
         serviciosCerca.add("Gimnasio");
         
         ObservableList servicios = FXCollections.observableList(serviciosCerca);     
-        this.serviciosCerca.setItems(servicios);
+        this.serviciosCerca.setItems(servicios);       
         
-        //Imagenes mostradas de la vivienda        
-        for (int i = 0; i < 3; i++){            
-            imageList.getChildren().add(galeria("/trobify/views/_FI.jpg"));        
+        //Boton de volver atras        
+            //volver.setOnAction(e -> scenePropia.setScene(scenaPrevia));      
+                
+        //Generador de fotos de la galeria       
+        for(int i = 0; i < listaFotos.size(); i++){
+            try{
+                this.imageList.getChildren().add(galeria(listaFotos.get(i)));
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(FavoritosController.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
         
-        //Viviendas recomendadas
-        for (int i = 0; i < 3; i++){
-            recomendados.getChildren().add(galeria("/trobify/views/_FI.jpg"));        
+        //Mostrar la descripcion de la vivienda
+        this.descripcion.setText(consultarDescripcion(this.id));
+        
+        //Viviendas recomendadas        
+        for (int i = 0; i < listaRecomendados.size(); i++){
+            Button botonRecomendacion = new Button();
+            botonRecomendacion.setPadding(new Insets(0, 0, 0, 0));
+            botonRecomendacion.setId("botonRecomendacion" +i);
+            try{
+                ImageView fotoBoton = galeria(listaRecomendados.get(i)); 
+                botonRecomendacion.setGraphic(fotoBoton);
+                recomendados.getChildren().add(botonRecomendacion);
+                System.out.println(botonRecomendacion);
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(FavoritosController.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
+        
+        //Solamente pueden ponerse valorenumericos en el textfield
+        valorValoracion.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, 
+            String newValue) {
+                if (!newValue.matches("\\d*")) {
+                valorValoracion.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
         
     }    
     
-    //Generador de la galeria de fotos
-    private javafx.scene.image.ImageView galeria(String source){                
-        Image image = new Image(source);
+    //Generador de listaFotos
+    private void crearListaFotos(String id){
+        try {
+            Statement stm = con.getConnection().createStatement();
+            ResultSet rsl = stm.executeQuery ("SELECT id FROM fotografia WHERE id_vivienda = '" + id + "'");
+            if(rsl.first()){
+                rsl.beforeFirst();
+                while (rsl.next()) {
+                    this.listaFotos.add(rsl.getString("id"));
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(InicioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    //Generador de listaFotos
+    private void crearListaRecomendados(String id){
+        
+        int precioBase = consultarPrecio(id);
+        int precioAlto = precioBase + 150;
+        int precioBajo = precioBase - 150;
+        int i = 0;
+        try {
+            Statement stm = con.getConnection().createStatement();
+            ResultSet rsl = stm.executeQuery ("SELECT f.id FROM fotografia f WHERE f.id_vivienda IN (SELECT v.id FROM vivienda v WHERE v.id NOT LIKE '" + this.id + "' AND v.precio BETWEEN '" + precioBajo + "' AND '" + precioAlto + "')");
+            if(rsl.first()){
+                rsl.beforeFirst();
+                while(rsl.next() && i < 3){
+                    this.listaRecomendados.add(rsl.getString(1));
+                    i++;
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(InicioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }    
+
+    //Metodo para obtener y generar una foto a partir de una direccion
+    private javafx.scene.image.ImageView galeria(String source) throws FileNotFoundException{  
+        Image image = new Image(new FileInputStream(source));
         javafx.scene.image.ImageView fotoGaleria = new javafx.scene.image.ImageView(image);
         fotoGaleria.setFitWidth(200);
         fotoGaleria.setFitHeight(150);
@@ -94,29 +203,137 @@ public class FichaViviendaController implements Initializable {
         return fotoGaleria;
     }
     
-    private Boolean estaEnBD(String id) {
-    
-        Boolean comparacion = false;
-        /*
-        while(La tabla de favoritos tenga elementos) {
-            if(this.id == id de la vivienda de la BD que se esta comprobando) return true;
-            else pasamos a siguiente elemento de la lista;
+    //Metodo para saber el precio de la vivienda
+    public int consultarPrecio(String id){
+        
+        try {
+            Statement stm = con.getConnection().createStatement();
+            ResultSet rsl = stm.executeQuery("SELECT precio FROM vivienda WHERE id = '" + id + "'");
+            if (rsl.first()) return rsl.getInt(1);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(InicioController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        */
-        return comparacion;
+        return 0;
+    }
     
+    //Metodo para obtener la descripcion de la vivienda
+    public String consultarDescripcion(String id){
+        
+        String texto = "No lo dejes escapar.";
+        try {
+            Statement stm = con.getConnection().createStatement();
+            ResultSet rsl = stm.executeQuery("SELECT descripcion FROM vivienda WHERE id = '" + this.id + "'");
+            if (rsl.first() && rsl.getNString(1) != null) texto = rsl.getNString(1);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(InicioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return texto;
+    }
+    
+    //Comprobar si esta en favoritos
+    private Boolean estaEnFavoritos(String idVivienda) {
+        
+        this.estaEnFav = false;
+        
+        try {
+            Statement stm = con.getConnection().createStatement();
+            ResultSet rsl = stm.executeQuery ("SELECT id FROM favoritos WHERE id LIKE '" + this.id + "'");
+            if(rsl.first()){
+                this.estaEnFav = true;
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(InicioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return this.estaEnFav;
+    }
+    
+    //Muestra botones de favoritos
+    public void mostrarBotones(){
+        
+        if(this.estaEnFav){
+            valoracionGrupo.setVisible(true);
+            addFavoritos.setText("Quitar de favoritos");
+            logicaBotonesValoracion();
+            
+        } else {
+            valoracionGrupo.setVisible(false);
+            addFavoritos.setText("Añadir a favoritos");
+        }    
+    }
+    
+    private void logicaBotonesValoracion(){
+        if (valoracion == -1 || valoracion == 0){
+            textoValoracion.setText("Valoracion: ");
+            valorValoracion.clear();
+            valorValoracion.setVisible(true);
+            editarValoracion.setText("Añadir");
+        }
+        else{
+            textoValoracion.setText("Valoracion: " + this.valoracion);
+            valorValoracion.setVisible(false);
+            editarValoracion.setText("Editar");
+        }
+    }
+    
+    private int estaValorado(){
+        
+        try {
+            Statement stm = con.getConnection().createStatement();
+            ResultSet rsl = stm.executeQuery("SELECT valoracion FROM favoritos WHERE id = '" + this.id + "'");
+            if (rsl.first()) this.valoracion = rsl.getInt(1);
+            System.out.println(valoracion);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(InicioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return this.valoracion;
+    }
+    
+    @FXML
+    private void addFav(ActionEvent event) throws IOException {
+        
+        if(!estaEnFav) {
+            try {
+                Statement stm = con.getConnection().createStatement();
+                stm.executeUpdate("INSERT INTO `favoritos`(`id`, `id_cliente`) VALUES ('" + this.id + "','" + 1 + "')");
+                this.estaEnFav = true;
+                mostrarBotones();
+            } catch (SQLException ex) {
+                Logger.getLogger(InicioController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else{
+            try {
+                Statement stm = con.getConnection().createStatement();
+                stm.executeUpdate("DELETE FROM favoritos WHERE id = '" + this.id + "'");
+                this.estaEnFav = false;
+                this.valoracion = -1;
+                mostrarBotones();
+            } catch (SQLException ex) {
+                Logger.getLogger(InicioController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        logicaBotonesValoracion();
     }
 
     @FXML
-    private void addFav(ActionEvent event) throws IOException {
-        /*
-        if(!estaEnBD(viviendaID)) {
-            añadimos la vivienda a la lista tabla de favoritos del usuario y lanza un mensaje diciendo que se ha añadido
-        } else lanza un mensaje diciendo que ya esta en favoritos
-        */
+    private void editarValoracion(ActionEvent event) {     
+        if(this.valoracion == -1/*AÑADIR*/){
+            this.valoracion = Integer.parseInt(valorValoracion.getText());
+            try {
+                Statement stm = con.getConnection().createStatement();
+                stm.executeUpdate("UPDATE `favoritos` SET `valoracion`='" + this.valoracion + "' WHERE id = '" + this.id + "'");
+                
+            } catch (SQLException ex) {
+                Logger.getLogger(InicioController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else{
+            this.valoracion = -1;
+        }
+        logicaBotonesValoracion();
     }
-    
-     public static void pasarStage(Stage m){
-         s = m;
-     }
 }
