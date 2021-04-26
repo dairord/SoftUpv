7ed110f2b4b3b797ba810f87ecc1be9de76c9431
Usuario <Usuario@LAPTOP-1P5FD8TL>
2021-04-26 22:38:05 +0200

    jj

---
+++ src/trobify/controlador/RegistrarUsuarioController.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trobify.controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author Usuario
+ */
+public class RegistrarUsuarioController implements Initializable {
+
+    @FXML
+    private CheckBox agenteCheck;
+    @FXML
+    private Button registrarmeButton;
+    @FXML
+    private Button foto;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void tarjeta(MouseEvent event) {
+    }
+
+    @FXML
+    private void Registrarme(ActionEvent event) {
+    }
+
+    @FXML
+    private void atras(ActionEvent event) {
+    }
+    
+}
