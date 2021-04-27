b2c1d31e83cea0c7dd23aa08690d5be1a98827ec
Usuario <Usuario@LAPTOP-1P5FD8TL>
2021-04-27 11:42:16 +0200

    jj

--- src/trobify/controlador/RegistrarUsuarioController.java
+++ src/trobify/controlador/RegistrarUsuarioController.java
@@ -8,6 +8,8 @@ package trobify.controlador;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,6 +17,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import trobify.Conectar;
@@ -29,8 +34,6 @@ public class RegistrarUsuarioController implements Initializable {
     @FXML
     private CheckBox agenteCheck;
     @FXML
-    private Button registrarmeButton;
-    @FXML
     private Button foto;
 
     Conectar con;
@@ -40,18 +43,54 @@ public class RegistrarUsuarioController implements Initializable {
     private static String vieneDe;
     private String direccion;
     private String dondeVa;
+    @FXML
+    private TextField username;
+    @FXML
+    private Label usuIncorrecto;
+    @FXML
+    private PasswordField contraseña;
+    @FXML
+    private PasswordField repetirContraseña;
+    @FXML
+    private Label contraIncorrecta;
+    @FXML
+    private TextField nombre;
+    @FXML
+    private TextField apellidos;
+    @FXML
+    private TextField dni;
+    @FXML
+    private TextField telefono;
+    @FXML
+    private Button registrarmeBoton;
+    @FXML
+    private Label errorText;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
        
+    //ruta para volver atras
         if(vieneDe.equals("buscador")){ 
            direccion = "/trobify/views/Buscador.fxml";
             dondeVa = "BuscadorController";}
        else {direccion = "/trobify/views/Inicio.fxml";
             dondeVa = "InicioController";}
     
+    //boton registrar desactivado hasta que haya datos
+         final BooleanBinding sePuedeBuscar = Bindings.isEmpty(username.textProperty())
+               .or(Bindings.isEmpty(contraseña.textProperty()))
+                 .or(Bindings.isEmpty(repetirContraseña.textProperty()))
+                 .or(Bindings.isEmpty(nombre.textProperty()))
+                 .or(Bindings.isEmpty(apellidos.textProperty()))
+                 .or(Bindings.isEmpty(dni.textProperty()))
+                 .or(Bindings.isEmpty(telefono.textProperty()))
+                 ;
+                
+          registrarmeBoton.disableProperty().bind(sePuedeBuscar);
+       con = new Conectar();
+        
     }    
 
    
